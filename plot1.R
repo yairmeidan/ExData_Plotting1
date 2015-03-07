2a34e10ba7a039aad3ff16242dfe7cc13f2f9e11
Yair <yair.meidan@gmail.com>
2015-03-07 22:56:14 +0200

    R code for 1st plot

---
+++ plot1.R
@@ -0,0 +1,38 @@
+## download zip file
+fileUrl<-"https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
+download.file(fileUrl,destfile="household_power_consumption.zip")
+
+## read required lines only, from specific dates
+d<-subset(read.table(
+  unz("household_power_consumption.zip", "household_power_consumption.txt")
+  ,header=TRUE
+  ,sep=";"
+  ,na.strings="?"
+  ,stringsAsFactors=FALSE
+)
+,Date %in% c("1/2/2007","2/2/2007")
+,select="Global_active_power"
+)
+
+##########
+## plot 1
+##########
+
+png(
+  file="plot1.png"
+  ,width = 480
+  ,height = 480
+  , units = "px"
+)
+
+hist(
+  d$Global_active_power
+  ,col="red"
+  ,main="Global Active Power"
+  ,xlab="Global Active Power (kiloWatts)"
+  ,ylab="Frequency"
+  ,border=TRUE
+  ,bty="l"
+)
+
+dev.off()
